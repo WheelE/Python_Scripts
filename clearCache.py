9b6c78fcc768586a7a75d4f7d55bfec649adfe61
WheelE <emaly3780@gmail.com>
2023-09-30 16:05:16 -0500

    cache folder function

--- clearCache.py
+++ clearCache.py
@@ -11,16 +11,18 @@ for folder, subfolders, files in os.walk(cache_path):
     if folder != cache_path:
         for file in files:
             if re.search('Cook.+',file):
-                os.remove(file)
-                print("Cookies Cleared")
-            else:
-                print("No Cookies Found")
-        
-        if re.search('Cach.+',folder):
-            print("Cache folders found " + folder)
-            """
-            for name in os.listdir(folder):
-                file = folder + name
-                if os.path.isfile(file):
-                    os.remove(file)
-                    """
+                print("Cookies found at " + folder)
+                for name in os.listdir(folder):
+                    file = folder + name
+                    if os.path.isfile(file):
+                            os.remove(file)
+#                    else:
+#                        print("No Cookies Found")
+    if re.search('Cach.+',folder):
+        print("Cache folders found " + folder)
+            
+    for name in os.listdir(folder):
+        file = folder + name
+        if os.path.isfile(file):
+            os.remove(file)
+                    
